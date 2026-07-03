e496192030fd74aebcfdc844da425c4d2472423f
a-7th <aelamri24@gmail.com>
2026-07-03 15:15:08 +0100

    Ohun elo idanwo 13 akọkọ ki a le mura silẹ fun adagun-odo ni oṣu ti n bọ

--- trello/1_lvl/05-repeat_alpha/repeat_alpha.c
+++ trello/1_lvl/05-repeat_alpha/repeat_alpha.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 int main(int ac, char **av){
-    char c;
-    int count;
     int i = 0;
-    if (ac != 2){
+    int count;
+    char c;
+    if(ac != 2){
         write(1, "\n", 1);
         return(0);
     }
@@ -15,11 +15,10 @@ int main(int ac, char **av){
             count = c - 'a' + 1;
         else if(c >= 'A' && c <= 'Z')
             count = c - 'A' + 1;
-        else
+        else 
             count = 1;
-        while(count--){
+        while(count--)
             write(1, &c, 1);
-        }
         i++;
     }
     write(1, "\n", 1);
