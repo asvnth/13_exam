fc7c5f9d07d2a96dbf29edc5e9bdeee238287d54
a-7th <aelamri24@gmail.com>
2026-07-04 00:09:07 +0100

    Ohun elo idanwo 13 akọkọ ki a le mura silẹ fun adagun-odo ni oṣu ti n bọ

---
+++ trello/2_lvl/02-swap_bits/swap_bits.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+unsigned char	swap_bits(unsigned char oct)
+{
+	return ((oct >> 4) | (oct << 4));
+}
+
+unsigned char swap_bits(unsigned char octet);
+
+int main(void)
+{
+    unsigned char a = 14;
+    unsigned char b;
+
+    printf("before: %u\n", a);
+    b = swap_bits(a);
+    printf("after : %u\n", b);
+
+    return 0;
+}
+
+/*
+#include <unistd.h>
+
+unsigned char swap_bits(unsigned char octet);
+
+void print_bits(unsigned char octet)
+{
+    int i = 8;
+    while (i--)
+    {
+        char c = ((octet >> i) & 1) + '0';
+        write(1, &c, 1);
+    }
+}
+
+int main(void)
+{
+    unsigned char a = 14;  // 00001110
+    unsigned char b;
+
+    print_bits(a);
+    write(1, "\n", 1);
+
+    b = swap_bits(a);
+
+    print_bits(b);
+    write(1, "\n", 1);
+
+    return (0);
+}
+*/
