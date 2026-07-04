86c93af94b5875cdd67f661643da58084d2afd3c
a-7th <aelamri24@gmail.com>
2026-07-04 20:57:38 +0100

    Ohun elo idanwo 13 akọkọ ki a le mura silẹ fun adagun-odo ni oṣu ti n bọ

--- trello/2_lvl/05-max/max.c
+++ trello/2_lvl/05-max/max.c
@@ -1,10 +1,8 @@
-#include <unistd.h>
-
 int max(int* tab, unsigned int len){
-    int result = tab[0];
     unsigned int i = 1;
     if (len == 0)
         return(0);
+    int result = tab[0];
     while (i < len){
         if (tab[i] > result)
             result = tab[i];
