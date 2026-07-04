bd7006d5df8c1d14bdb898bc2ea17dcf18b22c44
a-7th <aelamri24@gmail.com>
2026-07-04 23:40:00 +0100

    Ohun elo idanwo 13 akọkọ ki a le mura silẹ fun adagun-odo ni oṣu ti n bọ

---
+++ trello/2_lvl/08-inter/inter.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int main(int ac, char **av){
+    if (ac != 3){
+        write(1, "\n", 1);
+        return (0);
+    }
+    int seen[256] = {0};
+    char *s1 = av[1];
+    while (*s1){
+        char *s2 = av[2];
+        if (!seen[(unsigned char)*s1]){
+            while (*s2 && *s1 != *s2)
+                s2++;
+            if(*s2 == *s1)
+                write(1, s1, 1);
+        }
+        seen[(unsigned char)*s1] = 1;
+        s1++;
+    }
+    write(1, "\n", 1);
+    return (0);
+}
