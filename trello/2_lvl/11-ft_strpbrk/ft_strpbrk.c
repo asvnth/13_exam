e60ee75edf2ca3fd304ad785f2cfdf9728f99688
a-7th <aelamri24@gmail.com>
2026-07-05 16:45:17 +0100

    Ohun elo idanwo 13 akọkọ ki a le mura silẹ fun adagun-odo ni oṣu ti n bọ

---
+++ trello/2_lvl/11-ft_strpbrk/ft_strpbrk.c
@@ -0,0 +1,12 @@
+char *strpbrk(const char *s1, const char *s2){
+    while (*s1){
+        const char *p = s2;
+        while (*p){
+            if (*s1 == *p)
+                return ((char *)s1);
+            p++;
+        }
+        s1++;
+    }
+    return (0);
+}
