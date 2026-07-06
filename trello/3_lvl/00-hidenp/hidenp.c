4caad9fde17f56d7d0ca1ff87a4b735bffd5ff58
a-7th <aelamri24@gmail.com>
2026-07-06 18:30:25 +0100

    add 68-weather_app/ a fine app with different concepts perfect for a fine basics end

--- trello/3_lvl/00-hidenp/hidenp.c
+++ trello/3_lvl/00-hidenp/hidenp.c
@@ -5,5 +5,16 @@ int main(int ac, char **av){
         write(1, "\n", 1);
         return (0);
     }
+    int i = 0;
+    int j = 0;
+    while (av[1][i] && av[2][j]){
+        if (av[1][i] == av[2][j])
+            i++;
+        j++;
+    }
+    if (!av[1][i])
+        write(1, "1\n", 2);
+    else 
+        write(1, "0\n", 2);
     return (0);
 }
