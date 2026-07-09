298c31c2133bbee96d158ac80a181647580d057c
a-7th <aelamri24@gmail.com>
2026-07-09 15:33:01 +0100

    add 68-weather_app/ a fine app with different concepts perfect for a fine basics end

--- trello/3_lvl/07-tab_mult/tab_mult.c
+++ trello/3_lvl/07-tab_mult/tab_mult.c
@@ -1,23 +1,31 @@
 #include <unistd.h>
 
 int ft_atoi(char *s){
-    int f = 0;
+    int n = 0;
     int sign = 1;
     if (*s == '-'){
         sign = -1;
         s++;
     }
-    while(*s >= '0' && *s <= '9'){
-        f = f * 10 + (*s++ - '0');
-    }
-    return(f * sign);
+    while(*s >= '0' && *s <= '9')
+        n = n * 10 + (*s++ - '0');
+    return(n * sign);
 }
 
 void ft_putnbr(int n){
-    if(n > 9)
+    char c;
+    if (n == -2147483648){
+        write(1, "-2147483648", 11);
+        return;
+    }
+    if (n < 0){
+        write(1, "-", 1);
+        n = -n;
+    }
+    if (n > 9)
         ft_putnbr(n/10);
-    n = (n % 10) + '0';
-    write(1, &n, 1);
+    c = (n % 10) + '0';
+    write(1, &c, 1);
 }
 
 int main(int ac, char **av){
@@ -25,12 +33,11 @@ int main(int ac, char **av){
         int i = 1;
         int n = ft_atoi(av[1]);
         while (i < 10){
-            int res = n * i;
             ft_putnbr(i);
             write (1, " * ", 3);
-            ft_putnbr(ft_atoi(av[1]));
+            ft_putnbr(n);
             write(1, " = ", 3);
-            ft_putnbr(res);
+            ft_putnbr(n * i);
             write(1, "\n", 1);
             i++;
         }
