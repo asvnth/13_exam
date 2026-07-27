04c830535c2bac6511965465a12f884b1054a8a8
@7th <122217936+a-7th@users.noreply.github.com>
2026-07-28 00:59:54 +0100

    Update ft_cnvrt_base.c

--- 42_exam-shell/ring/convrt_base/ft_cnvrt_base.c
+++ 42_exam-shell/ring/convrt_base/ft_cnvrt_base.c
@@ -1,14 +1,20 @@
-char *convert_base(const char *s, int from, int to) {
-    long n = strtol(s, NULL, from);
+char *ft_convert_base(char *nbr, char *base_from, char *base_to) {
+    int from = strlen(base_from), to = strlen(base_to);
+    long n = 0;
+    int neg = (*nbr == '-');
+    if (neg) nbr++;
+    for (; *nbr; nbr++)
+        n = n * from + (strchr(base_from, *nbr) - base_from);
+
     char tmp[65];
     char *p = tmp + 64;
     *p = '\0';
-    int neg = n < 0;
-    if (neg) n = -n;
+    long v = neg ? -n : n;
+    if (v < 0) v = -v;
     do {
-        *--p = "0123456789abcdefghijklmnopqrstuvwxyz"[n % to];
-        n /= to;
-    } while (n);
+        *--p = base_to[v % to];
+        v /= to;
+    } while (v);
     if (neg) *--p = '-';
 
     char *r = malloc(strlen(p) + 1);
