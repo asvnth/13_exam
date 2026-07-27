b231503548190a09b74e1c4a3bf0b71359bf2105
@7th <122217936+a-7th@users.noreply.github.com>
2026-07-28 00:59:00 +0100

    Update ft_str_join.c

--- 42_exam-shell/ring/str_join/ft_str_join.c
+++ 42_exam-shell/ring/str_join/ft_str_join.c
@@ -1,4 +1,15 @@
-char *str_join(const char *a, const char *b) {
-    char *r = malloc(strlen(a) + strlen(b) + 1);
-    return r ? strcat(strcpy(r, a), b) : NULL;
+char *ft_strjoin(int size, char **strs, char *sep) {
+    int sep_len = strlen(sep), total = 0;
+    for (int i = 0; i < size; i++)
+        total += strlen(strs[i]) + (i < size - 1 ? sep_len : 0);
+
+    char *r = malloc(total + 1);
+    if (!r) return NULL;
+
+    r[0] = '\0';
+    for (int i = 0; i < size; i++) {
+        strcat(r, strs[i]);
+        if (i < size - 1) strcat(r, sep);
+    }
+    return r;
 }
