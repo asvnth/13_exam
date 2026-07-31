4bee8e90cde55739fd5302e1ea0b5ae6d2662a82
@7th <122217936+a-7th@users.noreply.github.com>
2026-07-31 12:19:11 +0100

    Update trcci.c

--- 42_exam-shell/ring/trcci/trcci.c
+++ 42_exam-shell/ring/trcci/trcci.c
@@ -1,8 +1,8 @@
 int tribonacci(int n) {
-    if (n <= 0) return 0;
-    if (n == 1 || n == 2) return 1;
+    if (n <= 0 || n == 1) return 0;
+    if (n == 2) return 1;
 
-    int a = 0, b = 1, c = 1, i = 3, next;
+    int a = 0, b = 0, c = 1, i = 3, next;
 
     while (i <= n) {
         next = a + b + c;
