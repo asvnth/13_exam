a3dfe51068c58df71bee98be4ed39de08512af1d
@7th <122217936+a-7th@users.noreply.github.com>
2026-07-31 13:18:31 +0100

    Update itoa.c

--- 42_exam-shell/ring/itoa/itoa.c
+++ 42_exam-shell/ring/itoa/itoa.c
@@ -1,10 +1,13 @@
 char *itoa(int n, char *s){
     int i = 0, sign = n;
     if (sign < 0) n = -n;
-    do { s[i++] = n % 10 + '0'; } while ((n /= 10) > 0);
+    while (1) {
+        s[i++] = n % 10 + '0';
+        n /= 10;
+        if (n == 0) break;
+    }
     if (sign < 0) s[i++] = '-';
     s[i] = '\0';
-    // reverse
     for (int j = 0, k = i - 1; j < k; j++, k--) {
         char tmp = s[j]; s[j] = s[k]; s[k] = tmp;
     }
