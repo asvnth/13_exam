e3b592f81f07697e4524ede1d6704a51acf05e6f
@7th <122217936+a-7th@users.noreply.github.com>
2026-08-05 15:33:51 +0100

    Update rot_array.c

--- 42_exam-shell/ring/rot_arr/rot_array.c
+++ 42_exam-shell/ring/rot_arr/rot_array.c
@@ -1,29 +1,41 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-
-void rotate(int *a, int n, int k)
-{
-	int *t = malloc(sizeof(int) * n);
-	k = ((k % n) + n) % n;
-	for (int i = 0; i < n; i++)
-		t[i] = a[(i + k) % n];
-	for (int i = 0; i < n; i++)
-		a[i] = t[i];
-	free(t);
+void	ft_putnbr(int n){
+	char c;
+	if (n == -2147483648){
+		write(1, "-2147483648", 11);
+		return;
+	}
+	if (n < 0){
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
 }
-
-int main(int argc, char **argv)
-{
-	if (argc < 3) return 1;
-	int k = atoi(argv[1]);
-	int n = argc - 2;
+int	main(int ac, char **av){
+	if (ac < 3)
+		return (1);
+	int n = ac - 2;
+	int k = atoi(av[1]) % n;
 	int *a = malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++)
-		a[i] = atoi(argv[i + 2]);
-	rotate(a, n, k);
-	for (int i = 0; i < n; i++)
-		printf("%d ", a[i]);
-	printf("\n");
+	int *t = malloc(sizeof(int) * n);
+	int i = 0, j = 0;
+	for (i = 0; i < n; i++)
+		a[i] = atoi(av[i + 2]);
+	int start = n - k;
+	for (i = start; i < n; i++)
+		t[j++] = a[i];
+	for (i = 0; i < start; i++)
+		t[j++] = a[i];
+	for (i = 0; i < n; i++){
+		ft_putnbr(t[i]);
+		write(1, " ", 1);
+	}
+	write(1, "\n", 1);
 	free(a);
-	return 0;
+	free(t);
+	return (0);
 }
