a035294ce7b56735e9330a724e5c7b9345e15e2d
@7th <122217936+a-7th@users.noreply.github.com>
2026-08-10 22:36:27 +0100

    Update ft_climbingStairs.c

--- 42_exam-shell/ring/climbingStairs/ft_climbingStairs.c
+++ 42_exam-shell/ring/climbingStairs/ft_climbingStairs.c
@@ -1,36 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_climbingStairs.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ahmelamr <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/22 23:32:30 by ahmelamr          #+#    #+#             */
-/*   Updated: 2026/07/23 00:15:09 by ahmelamr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-long long	climbingStairs(int n){
-	if (n <= 1)
-		return 1;
-
-	long long prev2 = 1; // ways(0)
-	long long prev1 = 1; // ways(1)
-	long long curr  = 0;
-	int	i = 2;
-	while (i <= n) {
-		curr  = prev1 + prev2;
-		prev2 = prev1;
-		prev1 = curr;
-		i++;
+int clim(int n){
+	long t, a = 1, b = 1;
+	while (n--){
+		t = a;
+		a = b;
+		b += t;
 	}
-
-	return curr;
+	return a;
 }
-
 #include <stdio.h>
 int main() {
     int n = 10;
-    printf("Ways to climb %d stairs: %lld\n", n, climbingStairs(n));
+    printf("Ways to climb %d stairs: %d\n", n, clim(n));
     return 0;
 }
