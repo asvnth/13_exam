2380904c04afa5f240e8f2d2f283eeb9568ff594
@7th <122217936+a-7th@users.noreply.github.com>
2026-08-10 18:48:08 +0100

    Improve formatting of anagram function
    Refactor anagram function for better readability.

--- 42_exam-shell/ring/pair/xpair.c
+++ 42_exam-shell/ring/pair/xpair.c
@@ -1,1 +1,14 @@
-int anagram(char*a,char*b){char *p,*q;while(*a&&*b){p=a;q=b;while(*q&&*q!=*p)q++;if(!*q)return 0;while(*q){*q=*q[1];q++;}a++;}return!*a&&!*b;}
+int anagram(char*a,char*b){
+  char *p,*q;
+  while(*a&&*b){
+    p=a;q=b;
+    while(*q&&*q!=*p)
+      q++;
+    if(!*q) return 0;
+    while(*q){
+      *q=*q[1];q++;
+    }
+    a++;
+  }
+  return!*a&&!*b;
+}
